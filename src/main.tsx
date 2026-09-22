8f55292064f8fcc83b378be067378ef8cb86eaa6
SystemPlasma <ilinder.work@gmail.com>
2026-09-21 23:20:13 -0500

    Align all workspaces with Mission BBQ orange and charcoal palette

--- src/main.tsx
+++ src/main.tsx
@@ -3,4 +3,5 @@ import {createRoot} from 'react-dom/client';
 import './pwa';
 import './style.css';
 import {WorkspaceRoot} from './WorkspaceRoot';
+import './brand.css';
 createRoot(document.getElementById('root')!).render(<React.StrictMode><WorkspaceRoot/></React.StrictMode>);
